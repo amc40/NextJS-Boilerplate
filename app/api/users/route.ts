7d2c2d121303735eb1bc2893a576fbec6e4618ae
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2023-06-23 14:34:57 +0000

    changed to use api routes

---
+++ app/api/users/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+  const users = await queryBuilder
+    .selectFrom('users')
+    .select(['id', 'name', 'username', 'email'])
+    .where('name', 'like', `%${search}%`)
+    .execute();
+}
